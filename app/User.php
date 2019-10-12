07b96b2bbcd9c81cf2c305c01bae9017d31f4e50
Regis Freyd <djaiss@users.noreply.github.com>
2019-10-12 19:48:38 -0400

    wip to add ui

--- app/User.php
+++ app/User.php
@@ -6,8 +6,9 @@ use App\Models\Source;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
