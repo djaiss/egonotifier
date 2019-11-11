6ce1b3ff4c73e80b658001521e34b357b843a885
Regis Freyd <djaiss@users.noreply.github.com>
2019-11-10 21:11:23 -0500

    chore: add horizon

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(FetchAllSources::class)->everyFiveMinutes();
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
